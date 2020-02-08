001b469649ccd1e967ad8ee653f97cc4ebdaa633
Anes Marzooki <marzoukianas@gmail.com>
2020-02-08 11:49:04 +0100

    dropdown

--- src/components/firstPage/Register.js
+++ src/components/firstPage/Register.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+
 import {
   Button,
   Card,
-  CardBody,
   Col,
   Container,
   Form,
@@ -10,10 +10,51 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
+  CardBody,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
   Row
 } from "reactstrap";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      type: '',
+      dropdownOpen: new Array(6).fill(false)
+    };
+  }
+
+  // onChange = e => {
+  //   this.setState({ [e.target]: })
+  // }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2,
+      type: this.state.type
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  };
+
+  toggle(i) {
+    const newArray = this.state.dropdownOpen.map((element, index) => {
+      return index === i ? !element : false;
+    });
+    this.setState({
+      dropdownOpen: newArray
+    });
+  }
+  
   render() {
     return (
       <div className=' align-items-center'>
@@ -22,7 +63,7 @@ class Register extends Component {
             <Col>
               <Card className='mx-4'>
                 <CardBody className='p-4'>
-                  <Form>
+                  <Form onSubmit={this.onSubmit}>
                     <h1>Register</h1>
                     <p className='text-muted'>Create your account</p>
                     <InputGroup className='mb-3'>
@@ -32,6 +73,9 @@ class Register extends Component {
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input
+                        id='name'
+                        onChange={this.onChange}
+                        value={this.state.name}
                         type='text'
                         placeholder='Username'
                         autoComplete='username'
@@ -42,6 +86,9 @@ class Register extends Component {
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
                       <Input
+                        id='email'
+                        onChange={this.onChange}
+                        value={this.state.email}
                         type='text'
                         placeholder='Email'
                         autoComplete='email'
@@ -54,7 +101,10 @@ class Register extends Component {
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input
+                        onChange={this.onChange}
+                        id='password'
                         type='password'
+                        value={this.state.password}
                         placeholder='Password'
                         autoComplete='new-password'
                       />
@@ -66,12 +116,46 @@ class Register extends Component {
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input
+                        onChange={this.onChange}
+                        id='password2'
                         type='password'
+                        value={this.state.password2}
                         placeholder='Repeat password'
                         autoComplete='new-password'
                       />
                     </InputGroup>
-                    <Button color='success' block>
+                    <Dropdown
+                      direction='right'
+                      isOpen={this.state.dropdownOpen[0]}
+                      toggle={() => {
+                        this.toggle(0);
+                      }}
+                    >
+                      <DropdownToggle caret>Select your status</DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem
+                          onClick={() => this.handleChange(User)}
+                          dropDownValue='User'
+                        >
+                          User
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem
+                          onClick={() => this.handleChange(Pharmacy)}
+                          dropDownValue='Pharmacy'
+                        >
+                          Pharmacy
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem
+                          onClick={() => this.handleChange(Doctor)}
+                          dropDownValue='Doctor'
+                        >
+                          Doctor
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                    <Button type='submit' color='success' block>
                       Create Account
                     </Button>
                   </Form>
